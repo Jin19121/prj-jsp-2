2dbafd7cd8798b618b003df23d29ee16b690ad6f
Jin19121 <jinkim19121@gmail.com>
2024-10-29 10:53:25 +0900

    login to post

--- src/main/java/com/example/prjjsp2/controller/BoardController.java
+++ src/main/java/com/example/prjjsp2/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.prjjsp2.controller;
 
 import com.example.prjjsp2.dto.Board;
+import com.example.prjjsp2.dto.Member;
 import com.example.prjjsp2.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -20,7 +22,19 @@ public class BoardController {
     private final BoardService service;
 
     @GetMapping("new")
-    public void newBoard() {
+    public String newBoard(@SessionAttribute(value = "loggedIn", required = false)
+                           Member member, RedirectAttributes rttr) {
+        if (member == null) {
+            //로그인 안 한 상태
+            rttr.addFlashAttribute("message", Map.of(
+                    "type", "warning",
+                    "text", "You must login first!"
+            ));
+            return "redirect:/member/login";
+        } else {
+            //로그인 완료
+            return "board/new";
+        }
     }
 
     @PostMapping("new")
