99e9b3db22c83653e57942ce1bb1a73add8dc14d
Jin19121 <jinkim19121@gmail.com>
2024-10-29 14:35:14 +0900

    권한 테이블 생성

--- sql/member.sql
+++ sql/member.sql
@@ -11,3 +11,17 @@ CREATE TABLE member
 
 SELECT *
 FROM member;
+
+#권한 테이블
+CREATE TABLE access
+(
+    id   VARCHAR(50) REFERENCES member (id),
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id, name)
+);
+
+#관리자 계정 임명
+INSERT INTO access
+    (id, name)
+VALUES ('admin', 'admin'),
+       ('bdmin', 'admin');
